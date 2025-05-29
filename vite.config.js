092b8ec33647e6007b3abc8f0d8b09caf68fd6f2
dhruvsalterego <saagarnkashyap06@gmail.com>
2025-05-30 04:00:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/saagarkashyap/',  // <-- 🔥 add this line: your repo name with slashes
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
     include: ['react-tsparticles', 'tsparticles']
